bd51a205f7206718f5cb09aaccf652aeba9846df
Yamatteo <yamatteo@gmail.com>
2021-12-10 10:54:17 +0100

    Add tensorboard

--- blockwork/__main__.py
+++ blockwork/__main__.py
@@ -7,13 +7,15 @@ from options import opts
 from . import BlockworkVox2Vox
 from .logger import set_logger
 from argparse import Namespace
+from torch.utils.tensorboard import SummaryWriter
 
-opts["run_path"] = "test"
+opts["run_path"] = "runs"
 opts["lr"] = 0.005
 bvv = BlockworkVox2Vox(Namespace(**opts))
 
 os.makedirs(bvv.run_path, exist_ok=True)
 set_logger(bvv.run_path)
+writer = SummaryWriter()
 
 with open(bvv.run_path / "last_run.log", mode="w") as f:
     f.writelines(
@@ -29,4 +31,6 @@ with open(bvv.run_path / "last_run.log", mode="w") as f:
         ]
     )
 
-bvv.train(5, 1, -1)
+bvv.train(1, 1, -1)
+writer.add_graph(bvv.gen, bvv.dataloader[0][0].float().cuda())
+writer.close()
